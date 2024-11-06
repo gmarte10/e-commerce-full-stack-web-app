4d9ac70c63f3e1233ac8eaa1b1cd04a8359131db
gmarte10 <giancarlosmarte10@gmail.com>
2024-11-06 15:00:47 -0500

    simplified cart page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Button, Container, ListGroup, Nav, Navbar } from "react-bootstrap";
 import axiosInstance from "../components/api/axiosInstance";
-import { useNavigate } from "react-router-dom";
+import useLogout from "../hooks/useLogout";
+import useGetImage from "../hooks/useGetImage";
 
 interface Product {
   id: number;
@@ -9,19 +10,18 @@ interface Product {
   price: number;
   description: string;
   image: string;
-  imageData?: string;
+  imageData?: string | null;
 }
 
 const Cart = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const username = localStorage.getItem("username");
-  const navigate = useNavigate();
+  const logout = useLogout();
+  const getImage = useGetImage();
 
   const getProductsFromCart = async () => {
     try {
       const token = localStorage.getItem("token");
-      console.log("token: " + token);
-      console.log("username: " + username);
       const response = await axiosInstance.get<Product[]>(
         `/cartItem?username=${username}`,
         {
@@ -33,13 +33,12 @@ const Cart = () => {
       console.log(response.data);
       const productsWithImg = await Promise.all(
         response.data.map(async (product) => {
-          const imageData = await getImages(product.image);
+          const imageData = await getImage(product.image);
           return { ...product, imageData };
         })
       )
-
       setProducts(productsWithImg);
-      // console.log(products);
+      console.log(products);
     } catch (error) {
       console.log(error);
     }
@@ -66,8 +65,6 @@ const Cart = () => {
     const username = localStorage.getItem("username");
     try {
       const token = localStorage.getItem("token");
-      console.log("token: " + token);
-      console.log("username: " + username);
       const response = await axiosInstance.delete(
         `/cartItem/${username}/${id}`,
         {
@@ -84,50 +81,9 @@ const Cart = () => {
     }
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("username");
-    navigate("/login");
-  };
-
   useEffect(() => {
     getProductsFromCart();
   }, []);
-
-  const getImages = async (image: string) => {
-    try {
-      const token = localStorage.getItem("token");
-      const response = await axiosInstance.get(`/images/${image}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        responseType: "blob",
-      });
-      const base64Data = await new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result as string);
-        reader.onerror = reject;
-        reader.readAsDataURL(response.data);
-      });
-      return base64Data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const loadImages = async () => {
-    try {
-      const productsWithImages = await Promise.all(
-        products.map(async (product) => {
-          const imageData = await getImages(product.image);
-          return { ...product, imageData };
-        })
-      );
-      setProducts(productsWithImages);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <>
@@ -139,7 +95,7 @@ const Cart = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Button onClick={handleLogout}>Logout</Button>
+              <Button onClick={logout}>Logout</Button>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -150,7 +106,7 @@ const Cart = () => {
             <p>Name: {product.name}</p>
             <p>Price: {product.price}</p>
             <p>Description: {product.description}</p>
-            <img src={product.imageData} alt={product.image} />
+            {product.imageData && <img src={product.imageData} alt={product.image} />}
             <Button onClick={() => handleOnRemoveFromCart(product.id)}>
               Remove From Cart
             </Button>
